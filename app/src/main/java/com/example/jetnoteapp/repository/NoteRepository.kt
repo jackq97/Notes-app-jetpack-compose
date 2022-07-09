6d9777bb75a06fed26f5641f97b06ec3683dea57
jaskq <jasjassi97@gmail.com>
2022-07-09 12:52:39 +0530

    Update NoteRepository.kt

--- app/src/main/java/com/example/jetnoteapp/repository/NoteRepository.kt
+++ app/src/main/java/com/example/jetnoteapp/repository/NoteRepository.kt
@@ -1,4 +1,24 @@
 package com.example.jetnoteapp.repository
 
-class NoteRepository {
+import com.example.jetnoteapp.data.NoteDatabaseDao
+import com.example.jetnoteapp.model.Note
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+
+// gonna inject dependency for our note dao
+
+class NoteRepository @Inject constructor(private val noteDatabaseDao: NoteDatabaseDao) {
+
+    suspend fun addNote(note: Note) = noteDatabaseDao.insert(note)
+    suspend fun updateNote(note: Note) = noteDatabaseDao.update(note)
+    suspend fun deleteNote(note: Note) = noteDatabaseDao.deleteNote(note)
+    suspend fun deleteAllNotes() = noteDatabaseDao.deleteAll()
+    fun getAllNotes(): Flow<List<Note>> = noteDatabaseDao.getAllNotes()
+        .flowOn(Dispatchers.IO)
+        .conflate()
+
 }
